07ea92ca111785ef26e48cbb77d7b57820e143f3
kamarthis <kamurthisreekanth21@gmail.com>
2021-01-17 12:48:08 +0530

    comments feature added

--- src/main/java/ImageHoster/controller/ImageController.java
+++ src/main/java/ImageHoster/controller/ImageController.java
@@ -16,6 +16,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.time.LocalDate;
 import java.util.*;
 
 @Controller
@@ -51,6 +52,7 @@ public class ImageController {
         Image image = imageService.getImage(imageId);
         model.addAttribute("image", image);
         model.addAttribute("tags", image.getTags());
+        model.addAttribute("comments", image.getComments());
         return "images/image";
     }
 
@@ -104,8 +106,10 @@ public class ImageController {
             String error = "Only the owner of the image can edit the image";
             model.addAttribute("editError", error);
             model.addAttribute("tags", image.getTags());
+            model.addAttribute("comments", image.getComments());
             return "images/image";
         }
+        
         return "images/edit";
     }
 
@@ -159,6 +163,7 @@ public class ImageController {
             model.addAttribute("tags", image.getTags());
             return "images/image";
         }
+        
         imageService.deleteImage(imageId);
         return "redirect:/images";
     }
